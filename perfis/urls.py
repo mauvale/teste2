3b06cfad76ec0ded1260c08ebf41cec0ce875f2d
Thiago <thiagosordi3@gmail.com>
2017-12-23 13:17:08 -0200

    Modificação parte 2

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('perfis/<int:perfil>', views.exibir, name='perfil'),
+    path('perfis/<int:perfil>/convidar', views.convidar, name='convidar')
 ]
--- perfis/views.py
+++ perfis/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'perfis': Perfil.objects.all()})
+
+def exibir(request, perfil):
+
+    perfilCreated = Perfil.objects.get(id=perfil)
+
+
+    return render(request, 'perfil.html', {"perfil": perfilCreated})
+
+
+def convidar(request, perfil):
+    pass
